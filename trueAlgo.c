3cb7620c7faa067fc3218545e4bacd26cbea1a11
dtlnor <dtlnor@hotmail.com>
2019-04-25 09:04:13 +0900

    update all

--- trueAlgo.c
+++ trueAlgo.c
@@ -2,28 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
+	
 	int arr[argc - 1];
 	int n = argc - 1;
-	int i,j;
-	int x = 0;
+	int i, j;
+	int temp;
 	
 	for (i = 0; i < n; i++) {
 		arr[i] = atoi(argv[i+1]);
 	}
-
-	for (j = 0; j < n-1; j++){
-		
-		if (arr[j]>=arr[j+1])
-		{			
-			if (arr[j] >= x) x = arr[j];
-		}			
-		else{
-			if (arr[j+1] >= x) x = arr[j+1];
+	
+	//printf("%d %d %d %d %d\n", arr[0], arr[1],arr[2],arr[3],arr[4]);
+	
+	for (i = 0; i < n - 1; i += 1) {
+		for (j = 0; j < n - i - 1; j += 1) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
 		}
 	}
-	printf ("%d\n",x);
-    
-    return 0;
+	
+	//print result	
+	printf("%d,%d,%d,%d,%d", arr[0], arr[1],arr[2],arr[3],arr[4]);
+	
+	return 0;
+	
 }
